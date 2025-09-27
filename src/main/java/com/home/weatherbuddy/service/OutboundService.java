11a18cc0c0c8999ff0daa24688c0efe4f177c08b
diuguide <everett.diuguid@gmail.com>
2025-09-27 01:06:05 -0400

    resolve spotbugs

--- src/main/java/com/home/weatherbuddy/service/OutboundService.java
+++ src/main/java/com/home/weatherbuddy/service/OutboundService.java
@@ -2,11 +2,13 @@ package com.home.weatherbuddy.service;
 
 import com.home.weatherbuddy.model.StationInstance;
 import org.springframework.stereotype.Service;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 @Service
 public class OutboundService {
     private final MemoryService memoryService;
 
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "Constructor DI of Spring-managed singleton is intentional and safe")
     public OutboundService(MemoryService memoryService) {
         this.memoryService = memoryService;
     }
